7cb0dd19f91033ec83e0a3a4caa82621c8d72d38
LianSheng197 <ci.i.cxcvii@gmail.com>
2019-11-15 00:17:44 +0800

    force no cache

--- sp1.php
+++ sp1.php
@@ -4,6 +4,9 @@ require $_SERVER["DOCUMENT_ROOT"] . "/func/main.php";
 require $_SERVER["DOCUMENT_ROOT"] . "/func/special.php";
 
 Header("Content-type: image/png");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 date_default_timezone_set("Asia/Taipei");
 
 $ttf = $_SERVER["DOCUMENT_ROOT"] . "/ttf/WenQuanYiZenHeiMono-02.ttf";
