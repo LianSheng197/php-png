068b027f1da5b89313437d62d098f9858827ba37
LianSheng197 <ci.i.cxcvii@gmail.com>
2019-11-14 23:37:33 +0800

    Add some special functions.

---
+++ func/special.php
@@ -0,0 +1,41 @@
+<?php
+function getDNT()
+{
+    return (isset($_SERVER['HTTP_DNT']) && $_SERVER['HTTP_DNT'] == 1);
+}
+
+function getIP()
+{
+    if (!empty($_SERVER["HTTP_CLIENT_IP"])) {
+        $ip = $_SERVER["HTTP_CLIENT_IP"];
+    } elseif (!empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+        $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+    } else {
+        $ip = $_SERVER["REMOTE_ADDR"];
+    }
+
+    return ($ip == "::1") ? "127.0.0.1" : $ip;
+}
+
+function getRef()
+{
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        $result = $_SERVER["HTTP_REFERER"];
+    } else {
+        $result = "Error_No_Referer";
+    }
+
+    return $result;
+}
+
+function getTitle(string $url)
+{
+    $str = file_get_contents($url);
+    if (strlen($str) > 0) {
+        $str = trim(preg_replace('/\s+/', ' ', $str));
+        // ignore case
+        preg_match("/\<title\>(.*)\<\/title\>/i", $str, $title);
+
+        return $title[1];
+    }
+}
